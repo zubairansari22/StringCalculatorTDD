b641339fe79d24df083042503148f1c6a0b6f624
Zubair Ansari <juberansari22@gmail.com>
2024-06-27 18:03:31 +0530

    Add support for two comma-separated numbers

--- StringCalculatorTDD/src/main/java/StringCalculator.java
+++ StringCalculatorTDD/src/main/java/StringCalculator.java
@@ -4,6 +4,11 @@ public class StringCalculator {
 	    if (numbers.isEmpty()) {
 	        return 0;
 	    }
+	    if (numbers.contains(",")) {
+	        String[] nums = numbers.split(",");
+	        return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+	    }
 	    return Integer.parseInt(numbers);
 	}
+
 }
--- StringCalculatorTDD/src/test/java/StringCalculatorTest.java
+++ StringCalculatorTDD/src/test/java/StringCalculatorTest.java
@@ -13,4 +13,9 @@ public class StringCalculatorTest {
         assertEquals(1, StringCalculator.add("1"));
         assertEquals(5, StringCalculator.add("5"));
     }
+    @Test
+    public void testTwoNumbers() {
+        assertEquals(3, StringCalculator.add("1,2"));
+        assertEquals(9, StringCalculator.add("4,5"));
+    }
 }
